5bbce76ef43467736d587e41b6ed33d4f9c51156
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-03-10 20:07:04 +0530

    Create footer.php

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+  <!-- footer section starts -->
+  <div class="footer">
+        <div class="wrapper">
+         <p class="text-center">© 2022 www.WowFood- the heaven. All rights reserved.</p>
+         </div>  
+      </div>
+      <!-- footer section ends -->
+     
+   </body>
+</html>
