8c28d68c2323deacfabfa0fb5ab6959bf0521628
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-03-17 03:03:24 +0530

    Create footer.php

---
+++ partials-front/footer.php
@@ -0,0 +1,55 @@
+ <!-- footer Section Starts Here -->
+        <footer class="section-p1">
+        <div class="col">
+            <img src="icon.png" class="logo" alt="">
+            <h4>Contact Us</h4>
+            <p><strong>Address:</strong>N/562 Wellington Road, Street no. 32, San Francisco</p>
+            <p><strong>Phone:</strong>+01 222 365 467/ +91 9123467787</p>
+            <p><strong>Hours:</strong>10:00- 18:00, Mon-Sat</p>
+            <div class="follow">
+                <h4>Follow us</h4>
+                <div class="icon">
+                    <ul>
+                        <li><a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a></li>
+                        <li><a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a></li>
+                        <li><a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <h4>About</h4>
+            <a href="#">About us</a>
+            <a href="#">Delivery Information</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="#">contact us</a>
+        </div>
+
+        <div class="col">
+            <h4>My Account</h4>
+            <a href="#">Sign In</a>
+            <a href="#">View Cart</a>
+            <a href="#">My Wishlist</a>
+            <a href="#">Track my order</a>
+            <a href="#">Help</a>
+        </div>
+
+        <div class="col install">
+            <h4>Install App</h4>
+            <p>From App Store or Google Play</p>
+            <div class="row">
+                <img src="pay/app.jpg" alt="">
+                <img src="pay/play.jpg" alt="">
+            </div>
+            <p>Secured Payment Gateways</p>
+            <img src="pay/pay.png" alt="">
+        </div>
+
+        <div class="copyright"><p>© 2022 www.WowFood- the heaven. All rights reserved.</p></div>
+    </footer>
+    <!-- footer Section Ends Here -->
+
+  </body>
+</html>
