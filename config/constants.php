61f3a27f73502510593263dc8fac49d942d31a1f
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-12-29 02:57:22 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,11 +4,11 @@
 
 
    //creating constants to store non repeating values
-   define('SITEURL' , 'http://localhost/food-order/');
+   define('SITEURL' , 'http://localhost/order-git/');
    define('LOCALHOST', 'localhost');    //constants in php are in uppercase letters
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
-   define('DB_NAME', 'food-order');
+   define('DB_NAME', 'alph-village');
    $conn= mysqli_connect(LOCALHOST ,DB_USERNAME, DB_PASSWORD) or die(mysqli_error());  //database connection
    $db_select= mysqli_select_db($conn, DB_NAME) or die(mysqli_error());   //selecting database
 
