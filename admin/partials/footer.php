d8e98fa80a29c150de8ce5f8c37817bf08a55199
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-12-29 14:47:53 +0530

    Update footer.php

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
   <!-- footer section starts -->
   <div class="footer">
         <div class="wrapper">
-         <p class="text-center">© 2022 www.WowFood- the heaven. All rights reserved.</p>
+         <p class="text-center">© 2023 alphvillage- the heaven. All rights reserved.</p>
          </div>  
       </div>
       <!-- footer section ends -->
